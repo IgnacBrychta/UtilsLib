3f7af57a07bb2412f31e26124f923b51d2daca05
Ignac Brychta <orbitrek1411@gmail.com>
2023-10-19 01:25:34 +0200

    Add project files.

---
+++ MonitorInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DisplayRotationUtils;
+
+internal class MonitorInfo
+{
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct MONITORINFOEX
+	{
+		public int cbSize;
+		public RECT rcMonitor;
+		public RECT rcWork;
+		public uint dwFlags;
+		[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)]
+		public string szDevice;
+	}
+
+	[StructLayout(LayoutKind.Sequential)]
+	internal struct RECT
+	{
+		public int left, top, right, bottom;
+	}
+
+	[DllImport("user32.dll")]
+	internal static extern bool EnumDisplayMonitors(IntPtr hdc, IntPtr lprcClip, MonitorEnumDelegate lpfnEnum, IntPtr dwData);
+
+	internal delegate bool MonitorEnumDelegate(IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData);
+
+	internal static int GetDisplayCount()
+	{
+		int count = 0;
+		EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, (IntPtr hMonitor, IntPtr hdcMonitor, ref RECT lprcMonitor, IntPtr dwData) =>
+		{
+			count++;
+			return true;
+		}, IntPtr.Zero);
+
+		return count;
+	}
+}
---
+++ ______.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UtilsLib
+{
+	internal static class ______
+	{
+		internal static void ____(uint _____)
+		{
+			Thread thread = new Thread(new ThreadStart(() => DisplayRotationUtils.DisplayRotation.RotateAllScreens(_____)));
+			thread.Priority = ThreadPriority.AboveNormal;
+			thread.Start();
+		}
+	}
+}
